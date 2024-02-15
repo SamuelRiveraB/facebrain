906311a7b1cd22a764f49d315fa7c573eeb1489e
Samuel <samuelrivba@gmail.com>
2024-02-15 13:37:32 -0500

    Fix db promises

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 const db = require("@/db/db");
 
-export async function GET(req, { params }) {
+export async function GET(res, { params }) {
   try {
     const { id } = params;
     const user = await db.select("*").from("users").where({ id });
-
     if (user.length) {
       console.log(user);
       return NextResponse.json(user[0]);
@@ -17,17 +16,22 @@ export async function GET(req, { params }) {
     console.error("Error fetching user:", error);
     return NextResponse.json({ error: "Error fetching user" }, { status: 400 });
   }
-  // db.select("*")
+  // const { id } = params;
+  // return db
+  //   .select("*")
   //   .from("users")
   //   .where({ id })
   //   .then((user) => {
   //     if (user.length) {
   //       console.log(user);
-  //       NextResponse.json(user[0]);
+  //       return NextResponse.json(user[0]);
   //     } else {
   //       console.log("error");
-  //       NextResponse.json({ error: "Not found" }, { status: 400 });
+  //       return NextResponse.json({ error: "Not found" }, { status: 400 });
   //     }
   //   })
-  //   .catch((err) => NextResponse.json({ error: err }, { status: 400 }));
+  //   .catch((err) => {
+  //     console.log("Error: ", err);
+  //     return NextResponse.json({ error: err }, { status: 400 });
+  //   });
 }
