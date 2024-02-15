d8d58b0179e9c9cb01419759da1353f10a0c4182
Samuel <samuelrivba@gmail.com>
2024-02-14 20:32:40 -0500

    Change db Promises to Async Await

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,19 +1,33 @@
 import { NextResponse } from "next/server";
 const db = require("@/db/db");
 
-export function GET(req, { params }) {
-  const { id } = params;
-  db.select("*")
-    .from("users")
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        console.log(user);
-        NextResponse.json(user[0]);
-      } else {
-        console.log("error");
-        NextResponse.json({ error: "Not found" }, { status: 400 });
-      }
-    })
-    .catch((err) => NextResponse.json({ error: err }, { status: 400 }));
+export async function GET(req, { params }) {
+  try {
+    const { id } = params;
+    const user = await db.select("*").from("users").where({ id });
+
+    if (user.length) {
+      console.log(user);
+      return NextResponse.json(user[0]);
+    } else {
+      console.log("error");
+      return NextResponse.json({ error: "Not found" }, { status: 400 });
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json({ error: "Error fetching user" }, { status: 400 });
+  }
+  // db.select("*")
+  //   .from("users")
+  //   .where({ id })
+  //   .then((user) => {
+  //     if (user.length) {
+  //       console.log(user);
+  //       NextResponse.json(user[0]);
+  //     } else {
+  //       console.log("error");
+  //       NextResponse.json({ error: "Not found" }, { status: 400 });
+  //     }
+  //   })
+  //   .catch((err) => NextResponse.json({ error: err }, { status: 400 }));
 }
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -28,62 +28,107 @@ export function GET() {
 export async function POST(req, res) {
   const { email, name, password } = await req.json();
   const hash = bcrypt.hashSync(password, 10);
+
   if (name) {
-    db.transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
-        .into("login")
-        .returning("email")
-        .then((loginEmail) => {
-          return trx("users")
-            .returning("*")
-            .insert({
-              name: name,
-              email: loginEmail[0].email,
-              joined: new Date(),
-            })
-            .then((user) => {
-              console.log("Success");
-              NextResponse.json(user[0]);
-            });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    }).catch((err) => {
-      console.error(err);
-      NextResponse.json({ error: "Unable to join" }, { status: 400 });
-    });
-  } else {
-    db.select("email", "hash")
-      .from("login")
-      .where("email", "=", email)
-      .then((data) => {
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-        console.log(isValid);
-        if (isValid) {
-          return db
-            .select("*")
-            .from("users")
-            .where("email", "=", email)
-            .then((user) => {
-              console.log(user);
-              NextResponse.json(user[0]);
-            })
-            .catch((err) =>
-              NextResponse.json({ error: "Error" }, { status: 400 })
-            );
-        }
+    try {
+      const result = await db.transaction(async (trx) => {
+        const loginEmail = await trx
+          .insert({ hash: hash, email: email })
+          .into("login")
+          .returning("email");
+
+        const user = await trx("users").returning("*").insert({
+          name: name,
+          email: loginEmail[0].email,
+          joined: new Date(),
+        });
+        console.log(user);
+        return user;
+        // const test = await trx("login").select("*");
+
+        // if (test) {
+        //   await trx.commit();
+        //   console.log(test);
+        //   return NextResponse.json(test);
+        // } else {
+        //   return NextResponse.json({ error: "Transaction" }, { status: 400 });
+        // }
       });
-    const user = database.users.find(
-      (user) => user.email === email && user.password === password
-    );
-    if (user) {
-      return NextResponse.json({ msg: "Success" }, { status: 200 });
+      return NextResponse.json(result);
+    } catch (err) {
+      console.error(err);
+      return NextResponse.json({ error: "Unable to join" }, { status: 400 });
+    }
+    // db.transaction((trx) => {
+    //   trx
+    //     .insert({
+    //       hash: hash,
+    //       email: email,
+    //     })
+    //     .into("login")
+    //     .returning("email")
+    //     .then((loginEmail) => {
+    //       return trx("users")
+    //         .returning("*")
+    //         .insert({
+    //           name: name,
+    //           email: loginEmail[0].email,
+    //           joined: new Date(),
+    //         })
+    //         .then((user) => {
+    //           console.log("Success");
+    //           NextResponse.json(user[0]);
+    //         });
+    //     })
+    //     .then(trx.commit)
+    //     .catch(trx.rollback);
+    // }).catch((err) => {
+    //   console.error(err);
+    //   NextResponse.json({ error: "Unable to join" }, { status: 400 });
+    // });
+  } else {
+    const data = await db
+      .select("email", "hash")
+      .from("login")
+      .where("email", "=", email);
+
+    const isValid = bcrypt.compareSync(password, data[0].hash);
+    console.log(isValid);
+
+    if (isValid) {
+      const user = await db
+        .select("*")
+        .from("users")
+        .where("email", "=", email);
+
+      console.log(user);
+      return NextResponse.json(user[0]);
     } else {
-      return NextResponse.json({ error: "Error logging in" }, { status: 400 });
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 400 }
+      );
     }
   }
+
+  // db.select("email", "hash")
+  //   .from("login")
+  //   .where("email", "=", email)
+  //   .then((data) => {
+  //     const isValid = bcrypt.compareSync(password, data[0].hash);
+  //     console.log(isValid);
+  //     if (isValid) {
+  //       return db
+  //         .select("*")
+  //         .from("users")
+  //         .where("email", "=", email)
+  //         .then((user) => {
+  //           console.log(user);
+  //           NextResponse.json(user[0]);
+  //         })
+  //         .catch((err) =>
+  //           NextResponse.json({ error: "Error" }, { status: 400 })
+  //         );
+  //     }
+  //   });
 }
