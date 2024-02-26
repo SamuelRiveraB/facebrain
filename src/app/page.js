1ccbef381d429e0ff2d1318edc87cd8d8cd3d55f
Samuel <samuelrivba@gmail.com>
2024-02-26 17:11:13 -0500

    Try to fix postgres connection in docker

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,7 @@ export default function Home() {
 
   useEffect(() => {
     // redirect("/signin");
+    console.log("Hello");
     fetch("http://localhost:3000/api/users/")
       .then((res) => res.json())
       .then(console.log);
